35235224163667f6feab9ba5d181d528fee4f6c0
105353509 <105353509@L815WD220012.edu.cbe.ab.ca>
2022-09-15 15:28:46 -0600

    3:28PM

--- Chapter10/src/BreakAPlate.java
+++ Chapter10/src/BreakAPlate.java
@@ -1,6 +1,6 @@
 /*
 
-Program: BreakAPlate.java          Last Date of this Revision: September 14, 2022
+Program: BreakAPlate.java          Last Date of this Revision: September , 2022
 
 Purpose: game of probability, break all three plates for the first prize, break two plates for the consolation prize
 
@@ -14,13 +14,22 @@ Course: Computer Programming 30
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
+import java.awt.BorderLayout;
 
 public class BreakAPlate implements ActionListener{
 
-	private JFrame frame;
+	static final String FIRST_PRIZE = "tiger plush";
+	static final String CONSOLATION_PRIZE = "sticker";
+	JFrame frame;
+	JButton playButton;
+	JLabel plates, prizes;
 
 	/**
 	 * Launch the application.
@@ -49,18 +58,43 @@ public class BreakAPlate implements ActionListener{
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		
+		ImageIcon platesPic = new ImageIcon("src/plates.gif");
+		
+		ImageIcon p2Broke = new ImageIcon("src/plates_two_broken.gif");
+		
+		ImageIcon pABroke = new ImageIcon("src/plates_all_broken.gif");
+		
+		ImageIcon sticker = new ImageIcon("src/sticker.gif");
+		
+		ImageIcon tiger_plush = new ImageIcon("src/tiger_plush.gif");
+		
+		// ImageIcon placeholder = new ImageIcon("src/placeholder.gif");
+		
+		
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 450, 335);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JButton playButton = new JButton("Play");
+		playButton = new JButton("Play");
 		playButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		playButton.setBounds(165, 143, 89, 23);
+		frame.getContentPane().setLayout(null);
+		playButton.setBounds(174, 120, 89, 23);
 		frame.getContentPane().add(playButton);
+		
+		plates = new JLabel(platesPic);
+		plates.setHorizontalAlignment(SwingConstants.CENTER);
+		plates.setBounds(10, 11, 414, 98);
+		frame.getContentPane().add(plates);
+		
+		prizes = new JLabel();
+		prizes.setBounds(10, 164, 414, 121);
+		frame.getContentPane().add(prizes);
+		
 	}
 
 	@Override
